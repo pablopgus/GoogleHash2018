d8d02e68f7754218f8c29d07f6e10b0e2e1c21b1
JCastro\Castro\ggit <jcastrodte@gmail.com>
2018-03-01 19:20:35 +0100

    ride+problem

---
+++ problem.py
@@ -0,0 +1,10 @@
+def problem():
+    def __init__(self, R, C, F, N, B, T):
+        self.R = R
+        self.C = C
+        self.F = F
+        self.N = N
+        self.B = B
+        self.T = T
+
+
---
+++ ride.py
@@ -0,0 +1,29 @@
+def ride():
+    def __init__(self, a, b, x, y, s, f):
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+        self.s = s
+        self.f = f
+
+    def __eq__(self, other):
+        return (self.f, self.s) == (other.f, other.s)
+
+    def __ne__(self, other):
+        return (self.f, self.s) != (other.f, other.s)
+
+    def __lt__(self, other):
+        return (self.f, -self.s) < (other.f, -other.s)
+
+    def __le__(self, other):
+        return (self.f, -self.s) <= (other.f, -other.s)
+
+    def __gt__(self, other):
+        return (self.f, -self.s) > (other.f, -other.s)
+
+    def __ge__(self, other):
+        return (self.f, -self.s) >= (other.f, -other.s)
+
+    def __repr__(self):
+        return "%s %s" % (self.f, self.s)
