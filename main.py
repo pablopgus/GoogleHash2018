34914154eb9bb4e6b73d11c6468beac51e1ca5f5
PStark <pablopg@us.es>
2018-03-01 20:04:55 +0100

    Update.

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ import my_parser
 import problem
 import ride
 
-
-my_parser.run('a_example.in')
-
 def getLibreVehicle(vlis):
     for v in len(vlis):
         if v.r < 0:
             return v;
     return -1
-
-
-class
-
 
 class vehicle():
     def __init__(self,x=0,y=0,rideID=-1):
@@ -21,8 +14,9 @@ class vehicle():
         self.y = y
         self.r = rideID
 
-p = problem(2,4,2,3,2,10)
-p.rideList = [ride(0,0, 0, 1, 3, 2, 9), ride(1,1, 2, 1, 0, 0, 9), ride(2,2, 0, 2, 2, 0, 9)]
+
+# Acquire the problem.
+p = my_parser.run('a_example.in')
 
 p.rideList.sort()
 vlis = []
--- my_parser.py
+++ my_parser.py
@@ -11,12 +11,13 @@ def run(file):
     p = problem.problem(tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5])
 
     line = file_.readline()
+    i = 0
     while line:
         tmp = re.split(" ", line)
-        r = ride.ride(tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5])
+        r = ride.ride(i, tmp[0], tmp[1], tmp[2], tmp[3], tmp[4], tmp[5])
         p.append_ride(r)
         line = file_.readline()
-
+        i = i + 1
     return p
 
 
