511bac048827b26f7c6e04afe3f64dcf51e51c30
JCastro\Castro\ggit <jcastrodte@gmail.com>
2018-03-01 20:00:43 +0100

    vehicle et ally

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-print(s)
-print("hol")
-print(s[::-1])
+import problem
+import ride
+
+def getLibreVehicle(vlis):
+    for v in len(vlis):
+        if v.r < 0:
+            return v;
+    return -1
+
+
+class
+
+
+class vehicle():
+    def __init__(self,x=0,y=0,rideID=-1):
+        self.x = x
+        self.y = y
+        self.r = rideID
+
+p = problem(2,4,2,3,2,10)
+p.rideList = [ride(0,0, 0, 1, 3, 2, 9), ride(1,1, 2, 1, 0, 0, 9), ride(2,2, 0, 2, 2, 0, 9)]
+
+p.rideList.sort()
+vlis = []
+for element in range(p.F):
+    vlis.append(vehicle)
+contp = 0
+for t in range(p.T):
