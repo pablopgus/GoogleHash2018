f8c660bf50afcc968325c37ac72bbfab6f49c27f
JCastro\Castro\ggit <jcastrodte@gmail.com>
2018-03-01 20:21:33 +0100

    avance 1, 2

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import problem
 import ride
 
+
 def getLibreVehicle(vlis):
     for v in len(vlis):
         if v.r < 0:
-            return v;
+            return v
     return -1
 
+def getRide():
+    contp += 1
+    return p.rideList[contp]
 
-class
+
+def assign():
+    v = getLibreVehicle()
+    while v > -1:
+        ride = getRide()
+        v.r = ride.id
+        v=getLibreVehicle()
+
+
+
+
+class solution():
+    def __init__(self,F):
+        for element in range(F):
+            self.vehicles.append([])
+
+
 
 
 class vehicle():
@@ -19,10 +39,15 @@ class vehicle():
 
 p = problem(2,4,2,3,2,10)
 p.rideList = [ride(0,0, 0, 1, 3, 2, 9), ride(1,1, 2, 1, 0, 0, 9), ride(2,2, 0, 2, 2, 0, 9)]
+contp = -1
 
 p.rideList.sort()
 vlis = []
 for element in range(p.F):
     vlis.append(vehicle)
-contp = 0
+
 for t in range(p.T):
+    assign()
+    avance()
+
+
